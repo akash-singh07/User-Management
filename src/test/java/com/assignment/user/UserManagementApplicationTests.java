b94396de0865fdce9cd36285ef944760ce9bc99d
akash <as098371@gmail.com>
2022-05-27 18:56:16 +0530

    Added some code to test

--- src/test/java/com/assignment/user/UserManagementApplicationTests.java
+++ src/test/java/com/assignment/user/UserManagementApplicationTests.java
@@ -1,5 +1,6 @@
 package com.assignment.user;
 
+import com.assignment.user.controller.UserController;
 import com.assignment.user.exception.ResourceNotFoundException;
 import com.assignment.user.model.User;
 import com.assignment.user.repository.UserRepository;
@@ -20,30 +21,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserManagementApplicationTests {
 
 	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private UserService userService;
+	private UserController userController;
 
 	@Test
-	@Order(1)
-	void createUser() throws Exception {
-		User user = new User("akash", "Akash", "Singh", "0123456789", "akash@gmail.com", "abc", "abc");
-		userService.createUser(user);
-		User user1 = userRepository.findByUserName("akash");
-		assertNotNull(user1);
-	}
-
-	@Test
-	@Order(2)
-	void deleteUser() {
-		User user = userRepository.findByUserName("akash");
-		Long id = user.getUserID();
-		userService.deleteUser(id);
-		Exception exception = assertThrows(ResourceNotFoundException.class,() -> {userService.getUser(id);});
-		String expectedMessage = "User does not exist";
-		String actualMessage = exception.getMessage();
-		assertTrue(actualMessage.contains(expectedMessage));
+	public void sortTest() {
+		userController.bubbleSort();
 	}
 
 }
